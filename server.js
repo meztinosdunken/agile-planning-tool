0ef4466a86436417d87216fdf9d204a04f078096
meztinosdunken <orlanmorry5462@hotmail.com>
2025-11-03 04:42:39 -0600

    Update response message from 'Hello, World!' to 'Welcome to my server!'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Welcome to my server!');
 });
 
 app.listen(PORT, () => {
